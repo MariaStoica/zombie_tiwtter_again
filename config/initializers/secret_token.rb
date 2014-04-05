784e9989d45147ef3241800d4c4f51854da40564
Maria Stoica <maria.stoica13@gmail.com>
2014-04-05 10:11:41 +0300

    initial second zombiestwitter app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZombieTwitter::Application.config.secret_key_base = '49246be851f77ecbcc0c09eafff152d12bcca654ecfd6bb9e82e5ee9b2019676c64a4e2b291ac882e9270c0f663ee745b2d60bb5d663af1d83bd05ac7bd62304'
